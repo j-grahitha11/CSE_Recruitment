0ec0c8f546f2b90586bf0c75efe6e85bbd450891
j-grahitha11 <jgrahitha.11@gmail.com>
2026-04-09 22:07:29 +0530

    Update gps_reading.ino

--- GPS/gps_reading.ino
+++ GPS/gps_reading.ino
@@ -1,63 +1,44 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-// ====== Pins ======
-#define RX_PIN 4
-#define TX_PIN 3
+// GPS RX, TX
+SoftwareSerial GPS_SoftSerial(4, 3);
 
-SoftwareSerial GPS_SoftSerial(RX_PIN, TX_PIN); // RX, TX
 TinyGPSPlus gps;
 
-// ====== Setup ======
 void setup() {
-  Serial.begin(9600);          // Serial monitor
-  GPS_SoftSerial.begin(9600);  // GPS module baud rate
-  Serial.println("GPS Simulation Starting...");
+  Serial.begin(9600);
+  GPS_SoftSerial.begin(9600);
 }
 
-// ====== Loop ======
 void loop() {
-  smartDelay(1000); // Delay 1 second while parsing GPS
+  smartDelay(1000);
 
-  // ----- Latitude & Longitude -----
-  if (gps.location.isValid()) {
+  if (gps.location.isValid() && gps.altitude.isValid()) {
+    
     Serial.print("Lat: ");
     Serial.println(gps.location.lat(), 6);
 
     Serial.print("Lon: ");
     Serial.println(gps.location.lng(), 6);
-  } else {
-    Serial.println("Lat: *****");
-    Serial.println("Lon: *****");
-  }
 
-  // ----- Altitude -----
-  if (gps.altitude.isValid()) {
     Serial.print("Altitude: ");
     Serial.print(gps.altitude.meters(), 2);
     Serial.println(" m");
-  } else {
-    Serial.println("Altitude: *****");
-  }
 
-  // ----- Time -----
-  if (gps.time.isValid()) {
-    char time_string[16];
-    sprintf(time_string, "Time: %02d:%02d:%02d",
-            gps.time.hour(), gps.time.minute(), gps.time.second());
-    Serial.println(time_string);
-  } else {
-    Serial.println("Time: *****");
+    Serial.println(); // spacing
   }
-
-  Serial.println("------");
+  else {
+    Serial.println("Waiting for GPS signal...");
+    Serial.println();
+  }
 }
 
-// ====== Helper: Delay while parsing GPS ======
 static void smartDelay(unsigned long ms) {
   unsigned long start = millis();
   do {
-    while (GPS_SoftSerial.available())
+    while (GPS_SoftSerial.available()) {
       gps.encode(GPS_SoftSerial.read());
+    }
   } while (millis() - start < ms);
 }
